a8db4db1b0741a3aaa2c2ce6554190dcc1a27347
Pavel Pantus <pavel@usebutton.com>
2016-08-20 10:20:09 -0400

    Initial commit

---
+++ Pictures/PicturesCollectionController.swift
@@ -0,0 +1,48 @@
+//
+//  PicturesCollectionController.swift
+//  Pictures
+//
+//  Created by Pavel Pantus on 8/20/16.
+//  Copyright © 2016 Pavel Pantus. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class PicturesCollectionController: UICollectionViewController,
+                                    UICollectionViewDelegateFlowLayout {
+
+    private let picturesCount = 100
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return picturesCount
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        cell.backgroundColor = UIColor.green
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+
+}
